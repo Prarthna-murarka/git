6a78b21d4e8b6f6bf9d26e3bec92bdadb112ffdf
Prarthna-murarka <chahat.murarka@gmail.com>
2017-01-15 15:11:08 +0530

    Update urls.py

--- virtualenviro/newproj/newproj/urls.py
+++ virtualenviro/newproj/newproj/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
 
     url(r'^admin/',include(admin.site.urls)),
-    url(r'^music/', include('music.urls')),
+  
     url(r'',include('blog.urls')),
 
 ]
